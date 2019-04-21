bdf39ae8058294428c4ae212dee5eb2ab004f3a0
Kaiwen Gong <gongkw@seagroup.com>
2019-04-21 22:21:33 +0800

    modify code

--- server_nodejs/db_database_creation.js
+++ server_nodejs/db_database_creation.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var con = mysql.createConnection({
   host: "127.0.0.1",
@@ -9,7 +9,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query("CREATE DATABASE Sports", function (err, result) {
+  con.query("CREATE DATABASE Sports", function(err, result) {
     if (err) throw err;
     console.log("Database created");
   });
--- server_nodejs/db_table_creation.js
+++ server_nodejs/db_table_creation.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var con = mysql.createConnection({
   host: "127.0.0.1",
@@ -10,8 +10,9 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE players ( user_id INT AUTO_INCREMENT PRIMARY KEY , name VARCHAR(255), team VARCHAR(255))";
-  con.query(sql, function (err, result) {
+  var sql =
+    "CREATE TABLE players ( user_id INT AUTO_INCREMENT PRIMARY KEY , name VARCHAR(255), team VARCHAR(255))";
+  con.query(sql, function(err, result) {
     if (err) throw err;
     console.log("Table created");
   });
